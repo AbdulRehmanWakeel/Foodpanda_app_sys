7ad1da14f2fa265ae3cf7a3b0af61249ef8ea264
Abdul Rehman <abdulrehman786268@gmail.com>
2025-09-04 13:27:53 +0500

    Update project with latest changes

---
+++ app/Models/Cart.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cart extends Model
+{
+    protected $fillable = ['user_id', 'restaurant_id'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function restaurant()
+    {
+        return $this->belongsTo(Restaurant::class);
+    }
+
+    public function items()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function totalPrice()
+    {
+        return $this->items->sum(fn($item) => $item->quantity * $item->price);
+    }
+}
---
+++ app/Models/CartItem.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class CartItem extends Model
+{
+    protected $fillable = ['cart_id', 'menu_id', 'quantity', 'price', 'addons'];
+
+    protected $casts = [
+        'addons' => 'array',
+    ];
+
+    public function cart()
+    {
+        return $this->belongsTo(Cart::class);
+    }
+
+    public function menu()
+    {
+        return $this->belongsTo(MenuItem::class, 'menu_id');
+    }
+}
